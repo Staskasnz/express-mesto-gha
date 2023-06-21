54aede9a4bd8f575332f08129c74466b739d45a7
Stanislav Alexandrov <staskasnz@mail.ru>
2023-06-21 23:30:32 +0300

    first7

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,18 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/:userId', getUserById);
+router.get('/:userId', celebrate({
+  body: Joi.object().keys({
+    _id: Joi.string().required().min(10),
+  }),
+}), getUserById);
 router.get('/me', getUserById);
-router.patch('/me', updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().pattern(/^https?:\/\/(?:www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+#?$/),
