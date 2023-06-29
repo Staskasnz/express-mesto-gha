b98153ae7db9a684ac7187fd28e62e91ba6a6543
Stanislav Alexandrov <staskasnz@mail.ru>
2023-06-29 17:06:52 +0300

    allowedCors

--- app.js
+++ app.js
@@ -14,13 +14,12 @@ const allowedCors = [
   'https://praktikum.tk',
   'http://praktikum.tk',
   'localhost:3000',
+  'https://staskasnz.nomoreparties.sbs/sign-in',
+  'https://api.staskasnz.nomoreparties.sbs/sign-in',
 ];
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
@@ -31,6 +30,9 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
